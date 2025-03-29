5a388e1858c995a35e11e03babb66f67a9f49120
SaynaHana <nwott1@gmail.com>
2025-03-29 12:45:16 -0400

    Added CSS styling to login and register pages

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 const PORT = process.env.PORT || 3000
 
 // View engine setup
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
