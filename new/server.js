40f3c2e06589f6251977ff4aceb25dd40df4d4d1
SaynaHana <nwott1@gmail.com>
2025-04-06 11:11:15 -0400

    Added dashboard page

---
+++ new/server.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.locals.pretty = true;
+
+var routes = require("./routes/index");
+
+app.use(routes.authenticate);
+app.use(logger("dev"));
+
+app.get("/dashboard", routes.dashboard);
+
+app.listen(PORT, err => {
+    if(err) console.log(err);
+    else {
+        console.log(`Server listening on port: ${PORT} CTRL-C to stop.`);
+    }
+})
